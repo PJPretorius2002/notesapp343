8a4c1ad5cc82139ed3954f4793bd07688daa1ebf
PJ Pretorius <24715980@sun.ac.za>
2023-10-12 22:22:27 +0200

    Update usersController.js

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,6 +3,19 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const knex = require('../../config/db'); // Adjust the path based on your file structure
 
+// Function to generate a valid bcrypt salt
+async function generateSalt() {
+  const saltRounds = 10; // You can adjust the number of salt rounds as needed
+  const salt = await bcrypt.genSalt(saltRounds);
+  return salt;
+}
+
+// Function to hash the password using bcrypt with the provided salt
+async function hashPassword(password, salt) {
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+}
+
 class UsersController {
   async register(req, res) {
     try {
@@ -13,8 +26,8 @@ class UsersController {
         return res.status(400).json({ message: "Invalid password format." });
       }
 
-      // Generate a salt
-      const salt = await generateSalt(16);
+      // Generate a valid bcrypt salt
+      const salt = await generateSalt();
 
       // Hash the password using bcrypt with the salt
       const hashedPassword = await hashPassword(password, salt);
@@ -46,18 +59,9 @@ class UsersController {
         return res.status(400).json({ message: "Invalid email or password." });
       }
 
-      // Combine the provided password with the stored salt
-      const combinedPassword = password + user.salt;
-
-      // Hash the combined password and salt
-      const hashedPasswordProvided = await hashPassword(combinedPassword, user.salt);
-
-      console.log('Provided password:', combinedPassword);
-      console.log('Stored hashed password:', user.password_hash);
-      console.log('Hashed password provided:', hashedPasswordProvided);
-
-      // Compare the hashed password provided with the stored hashed password
-      const validPassword = hashedPasswordProvided === user.password_hash;
+      // Hash the provided password with the stored salt and compare
+      const hashedPassword = await hashPassword(password, user.salt);
+      const validPassword = hashedPassword === user.password_hash;
 
       if (!validPassword) {
         return res.status(400).json({ message: "Invalid email or password." });
@@ -71,22 +75,6 @@ class UsersController {
       res.status(400).json({ message: error.message });
     }
   }
-}
-
-// Function to hash passwords using bcrypt
-async function hashPassword(password, salt) {
-  const hashedPassword = await bcrypt.hash(password, salt);
-  return hashedPassword;
-}
-
-// Replace with your actual salt generation logic
-async function generateSalt(length) {
-  const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  let salt = '';
-  for (let i = 0; i < length; i++) {
-    salt += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return salt;
 }
 
 const usersController = new UsersController();
