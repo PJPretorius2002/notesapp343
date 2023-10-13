18fa63c7da62f9b32aa0d7e8ad1d97d8a5a7eb90
PJ Pretorius <24715980@sun.ac.za>
2023-10-13 12:30:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://notesapp343-front-end-3ef1866fe1d5.herokuapp.com%27/);
+  res.header('Access-Control-Allow-Origin', 'https://notesapp343-front-end-3ef1866fe1d5.herokuapp.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
