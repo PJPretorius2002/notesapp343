fd70e07d33de90a590a34c17c4d1cae550036274
PJ Pretorius <24715980@sun.ac.za>
2023-10-15 16:29:46 +0200

    Update categoriesController.js

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,92 +1,89 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const knex = require('../../config/db'); // Adjust the path based on your file structure
+const db = require('../../config/db'); // Adjust the path based on your file structure
 
-class CategoriesController {
+class CategoriesApi {
+  async getCategories() {
+    const categories = await db('categories'); // Adjust the table name accordingly
+    return categories;
+  }
 
-  async getAllCategories(req, res) {
+  async createCategory(req, res) {
+    let userId;
+    if (req.user && req.user.id) {
+      userId = req.user.id;
+    } else {
+      return res.status(401).json({ error: 'User not authenticated' });
+    }
+
+    const category = {
+      name: req.body.name,
+      user_id: userId
+    };
+
     try {
-      const categories = await knex('categories');
-      res.status(200).json(categories);
+      const newCategoryIdArray = await db('categories').insert(category, ['category_id']);
+      const newCategoryId = newCategoryIdArray[0]?.category_id;
+
+      if (newCategoryId) {
+        const newCategory = await db('categories').where('category_id', newCategoryId).first();
+        return res.status(201).json(newCategory);
+      } else {
+        return res.status(500).json({ error: 'Failed to create category' });
+      }
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      console.error('Error inserting category:', error);
+      return res.status(500).json({ error: 'Failed to create category' });
     }
   }
-
-async createCategory(req, res) {
-  try {
-    const { name } = req.body;
-    const userId = req.user.id;  // Assuming your token includes the user ID
-
-    // Insert category details into the database, associating it with the user
-    const [categoryId] = await knex('categories').insert({
-      name,
-      user_id: userId  // Associate the category with the user
-    });
-    console.log('Insert response:', response);
-
-
-
-    // Check if categoryId is a valid number
-    if (typeof categoryId !== 'number' || isNaN(categoryId)) {
-      throw new Error('Invalid category ID');
-    }
-
-    console.log('categoryId:', categoryId);
-    const category = await knex('categories').where({ category_id: categoryId }).first();
-    console.log('category:', category);
-
-    res.status(201).json({ message: 'Category created successfully', category });
-  } catch (error) {
-    console.error('Error creating category:', error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-}
-
-
 
   // Add other category-related CRUD operations as needed
 }
 
-const categoriesController = new CategoriesController();
-const router = express.Router();
+const categoriesApi = new CategoriesApi();
+const router = express.Router(); // Create a router
+
 const secretKey = 'i9P&k6Xn2Rr6u9P2s5v8y/B?E(H+MbQe';
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.header('Authorization');
-  console.log('Auth Header:', authHeader);
 
-  const token = authHeader && authHeader.split(' ')[1]; // Extract the token without "Bearer "
-  console.log('Extracted Token:', token);
+  const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
-    console.log('Token not provided');
     return res.status(401).json({ message: 'Access denied' });
   }
 
-  // Decode the token to inspect its structure
   const decodedToken = jwt.decode(token);
-  console.log('Decoded token:', decodedToken);
 
   try {
     const decoded = jwt.verify(token, secretKey);
-    console.log('Decoded token:', decoded);  // Log the decoded token
     req.user = {
-      id: decoded.user_id  // Set req.user to an object containing user_id
+      id: decoded.user_id
     };
-    console.log('req.user:', req.user);
     next();
   } catch (err) {
-    console.log('Error verifying token:', err);
     return res.status(400).json({ message: 'Invalid token' });
   }
 };
 
-router.get('/', categoriesController.getAllCategories);
-router.post('/', authenticateToken, categoriesController.createCategory); // Use authenticateToken middleware
+router.use(authenticateToken);
+
+router.get("/", async (req, res) => {
+  const categories = await categoriesApi.getCategories();
+  res.status(200).json(categories);
+});
+
+router.post("/", async (req, res) => {
+  try {
+    await categoriesApi.createCategory(req, res);
+  } catch (error) {
+    console.error('Error creating category:', error);
+    res.status(500).json({ error: 'Failed to create category' });
+  }
+});
 
 module.exports = {
   authenticateToken,
   router
 };
-
