aff14fa29721db3db9ea35f1ec829e98bdd9865f
dalyjames1809 <85489859+dalyjames1809@users.noreply.github.com>
2023-10-16 11:44:54 +0200

    Update origin

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://notesapp343-front-end-3ef1866fe1d5.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
