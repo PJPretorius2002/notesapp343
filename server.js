27bc356e397ceb6cccafa4f3ada36a4567a0154b
PJ Pretorius <24715980@sun.ac.za>
2023-10-17 01:01:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io-client');
+const socketIo = require('socket.io');
 const { router: notesRouter, authenticateToken: notesAuthenticateToken } = require('./src/controllers/notesController');
 const { router: categoriesRouter, authenticateToken: categoriesAuthenticateToken } = require('./src/controllers/categoriesController');
 const usersController = require('./src/controllers/usersController');
