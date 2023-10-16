7a36ca0da4345b6f6e1ee8f98690272f1dbc4007
dalyjames1809 <85489859+dalyjames1809@users.noreply.github.com>
2023-10-16 12:04:20 +0200

    use.cors

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(cors());
 app.use(express.json());
 app.use('/notes', notesAuthenticateToken, notesRouter);
 app.use('/users', usersController);
