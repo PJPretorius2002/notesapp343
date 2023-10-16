1e811ca72b8839bb3b63ab57a96d3c8943afd3dc
dalyjames1809 <85489859+dalyjames1809@users.noreply.github.com>
2023-10-16 12:00:33 +0200

    Changed access control

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ const server = http.createServer(app);
 notesSetupWebSocket(server);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://notesapp343-front-end-3ef1866fe1d5.herokuapp.com');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 
